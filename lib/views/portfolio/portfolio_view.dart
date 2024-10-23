51a120fa441ee3b6ccfa1570181043fe9259fd50
jaaraarkey <olegudyachenko@gmail.com>
2024-10-23 16:55:35 +0200

    Portfolio page modified

--- lib/views/portfolio/portfolio_view.dart
+++ lib/views/portfolio/portfolio_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_basics/main.dart';
 
 class PortfolioView extends StatelessWidget {
   const PortfolioView({super.key});
@@ -6,10 +7,82 @@ class PortfolioView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-      child: Center(
-          child: Text('Portfolio View',
-              style: TextStyle(fontSize: 30, color: Colors.blueGrey))),
+      child: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: const Color.fromARGB(125, 248, 248, 248),
+              elevation: .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Image.asset('assets/images/placeholder.jpg'),
+                  Center(child: Text('Graphic Design')),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: const Color.fromARGB(125, 248, 248, 248),
+              elevation: .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Image.asset('assets/images/placeholder.jpg'),
+                  Center(child: Text('Ui/Ux Design')),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: const Color.fromARGB(125, 248, 248, 248),
+              elevation: .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Image.asset('assets/images/placeholder.jpg'),
+                  Center(child: Text('Development')),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: const Color.fromARGB(125, 248, 248, 248),
+              elevation: .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Image.asset('assets/images/placeholder.jpg'),
+                  Center(child: Text('Animartion')),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Card(
+              color: const Color.fromARGB(125, 248, 248, 248),
+              elevation: .2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Image.asset('assets/images/placeholder.jpg'),
+                  Center(child: Text('Music Production')),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
